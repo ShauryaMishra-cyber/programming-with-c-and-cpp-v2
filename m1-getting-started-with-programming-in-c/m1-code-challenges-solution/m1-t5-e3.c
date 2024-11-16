5db6fad22ce080b47122c3649db12007406b2dc7
ShauryaMishra-cyber <shaurya93350@gmail.com>
2024-11-17 00:15:52 +0530

    Update m1-t5-e3.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -2,8 +2,8 @@
 
     int main() {
 
-        puts("I am Rahul.");
-        puts("I am 20 years old.");
+        puts("I am Shaurya Mishra.");
+        puts("I am 7 years old.");
 
         return 0;
     }
